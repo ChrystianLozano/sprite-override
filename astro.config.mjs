d2b371172f35af4378ea8b6660db09ecd7a3dd22
Chrystian <chrystian@Mac-mini-de-Chrystian.local>
2026-08-17 18:26:37 -0600

    feat: configurar CNAME y astro.config.mjs para subdominio fortnite.mudo.games

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ChrystianLozano.github.io',
-  base: '/sprite-override',
+  site: 'https://fortnite.mudo.games',
+  base: '/',
 });
